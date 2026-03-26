09ced2737adf4560e834b0ee01c5fb1a56e9cca1
jey3dayo <nakazato_junya@ca-adv.co.jp>
2026-03-26 23:05:29 +0900

    feat: add Reeder-compatible keyboard shortcuts

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -1,6 +1,9 @@
+import { useKeyboard } from "../hooks/use-keyboard";
 import { AppLayout } from "./AppLayout";
 
 export function AppShell() {
+  useKeyboard();
+
   return (
     <div style={{ height: "100%", display: "flex", flexDirection: "column" }}>
       <AppLayout />
