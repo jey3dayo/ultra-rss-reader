5bcc13835ccdea2ff95fb96639e907829586b13a
jey3dayo <nakazato_junya@ca-adv.co.jp>
2026-04-30 00:19:36 +0900

    refactor(dead-code): remove unused frontend helpers

--- src/constants/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./browser";
-export * from "./events";
-export * from "./platform";
-export * from "./reader";
-export * from "./storage";
-export * from "./ui-layout";
-export * from "./ui-runtime";
